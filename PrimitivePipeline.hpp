6b88a8f23272391412a788c9363a58a2289cb83d
ramyaprabhu-alt <t-rprabhu@microsoft.com>
2025-03-03 07:56:07 +0000

    fix to make it run

--- PrimitivePipeline.hpp
+++ PrimitivePipeline.hpp
@@ -4,11 +4,11 @@
 #include "oneapi/dnnl/dnnl.hpp"
 #include <vector>
 #include <unordered_map>
-
+#include <variant>  
 
 // Structure for a matrix multiplication operation (Keep this here)
 struct MatMulOperation {
-    dnnl::matmul primitive;
+    std::variant<dnnl::primitive, std::function<void()>> primitive;
     std::unordered_map<int, dnnl::memory> args;
 };
 
@@ -17,6 +17,7 @@ class PrimitivePipeline {
 public:
     void insert(const MatMulOperation& op);
     void execute(dnnl::engine& eng, dnnl::stream& strm);
+    void insert_custom(const std::function<void()>& custom_func);  // Add this!
     void append(const PrimitivePipeline& other);
     MatMulOperation* get_last_operation() {
         return operations.empty() ? nullptr : &operations.back();
@@ -26,3 +27,6 @@ private:
 };
 
 #endif  // PRIMITIVE_PIPELINE_HPP
+
+
+    
